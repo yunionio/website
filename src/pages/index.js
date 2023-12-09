3968263af5b64628ccae1ea3a2d3b49707aa365b
Qiu Jian <qiujian@yunionyun.com>
2023-12-09 11:13:09 +0800

    fix: 1. homepage improvements 2. add ga and baidu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useColorMode } from '@docusaurus/theme-common';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageBrands from '@site/src/components/HomepageBrands';
@@ -14,21 +16,30 @@ import Translate, { translate } from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+  const { isDarkTheme } = useColorMode();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">
-          <Translate id="homepage.subtitle">开源、云原生的融合云平台</Translate>
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/">
-            <Translate id="homepage.getStarted">快速开始</Translate>
-          </Link>
+      <div className="container row">
+        <div className={clsx('col', styles.alignCenter)}>
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">
+            <Translate id="homepage.subtitle">云原生的开源融合云平台</Translate>
+          </p>
+          <p className="hero__title">
+            <Translate id="homepage.buildown">构建您自己的云上之云</Translate>
+          </p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/getting-started/">
+              <Translate id="homepage.getStarted">快速开始</Translate>
+            </Link>
+          </div>
+        </div>
+        <div className="col">
+          <img src={isDarkTheme? useBaseUrl('img/logo_black.png') : useBaseUrl('img/logo_white.png') } className={styles.heroImg} />
         </div>
       </div>
     </header>
@@ -36,7 +47,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  // const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={''}
--- tools/run-in-docker.sh
+++ tools/run-in-docker.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
+
+set -x
 
 docker run --rm -ti -v $(pwd):/website -w /website \
     --publish 3000:3000 \
-    --entrypoint yarn node:21.3.0-alpine3.17 "$@" # start --host 0.0.0.0
+    --entrypoint yarn node:21.3.0-alpine3.17 "$@"
