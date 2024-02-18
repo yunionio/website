ef6cd48bdaf700e05a18076aef8780500d94c2a7
Zexi Li <zexi.li@icloud.com>
2024-02-18 14:08:52 +0800

    Revert "feat: archive v3.10"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   url: 'https://www.cloudpods.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/v3.10/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
